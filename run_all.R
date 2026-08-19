4e17020ead1020a181896751e69f4f5a2a0b3922
Jacson Nascimento <jacson.nascimento@gmail.com>
2026-08-19 12:06:21 -0300

    Atualiza run_all para fluxo modular do Axion Lotofacil

--- run_all.R
+++ run_all.R
@@ -4,4 +4,9 @@
 # Projeto: Modelo Axion Lotofacil
 # ==========================================================
 
-source("R/Framework_Axion_Lotofacil_v1_2_standalone.R")
+source("R/00_config.R")
+source("R/01_pacotes_utilitarios.R")
+source("R/02_importacao_validacao.R")
+source("R/03_metricas_historicas.R")
+source("R/04_candidatos_residual.R")
+source("R/05_selecao_validacao_relatorio.R")
