b5bab36c420fd7bd26a2b3c26318781a1b69b731
Jacson Nascimento <jacson.nascimento@gmail.com>
2026-08-19 11:59:13 -0300

    Adiciona ponto de execucao unico Lotofacil v1.2

---
+++ run_all.R
@@ -0,0 +1,7 @@
+# ==========================================================
+# Execucao integral do Modelo Axion Lotofacil - v1.2
+# Autor: Jacson Cruz do Nascimento
+# Projeto: Modelo Axion Lotofacil
+# ==========================================================
+
+source("R/Framework_Axion_Lotofacil_v1_2_standalone.R")
